04c0090599d3ac099e095a408be1fc4178242769
Ederdal <degarcruz2004@gmail.com>
2025-10-21 23:05:44 -0600

    Practica 05
    add: se crearon los archivos de moviles_slideshow, home_screen, custom_appbar y widgets, todo esto para poder hacer un carrucel y mostrar las imagenes de las películas

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia_220219/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia_220219/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -32,14 +33,8 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    return Column(
+      children: [CustomAppbar(), MoviesSlideshow(movies: nowPlayingMovies)],
     );
   }
 }
